24c8bc804f5634823021dcb5ca48d5ca92df0b58
ranveerm <ranveer.mam@gmail.com>
2022-04-08 09:50:18 +1000

    ➕ Added weather conditions object

---
+++ MythicWeather/Models/WeatherConditions.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherConditions.swift
+//  MythicWeather
+//
+//  Created by Ranveer Mamidpelliwar on 7/4/2022.
+//
+
+import Foundation
+
+/**
+ Weather condition as described in [Weather Conditions](https://openweathermap.org/weather-conditions)
+ - Author: [ranveerm](https://github.com/ranveerm) 👨🏾‍💻
+ */
+struct WeatherConditions: Codable {
+    let id: Int
+    let type: WeatherType
+    let description: String
+    let icon: String
+}
