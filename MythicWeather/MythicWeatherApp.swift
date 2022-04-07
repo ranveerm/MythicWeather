3b78f7c4211b61ac9139b9ed8bf41a8e3118695e
ranveerm <ranveer.mam@gmail.com>
2022-04-07 19:42:05 +1000

    ➕ Added readme

---
+++ MythicWeather/MythicWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MythicWeatherApp.swift
+//  MythicWeather
+//
+//  Created by Ranveer Mamidpelliwar on 7/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct MythicWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
