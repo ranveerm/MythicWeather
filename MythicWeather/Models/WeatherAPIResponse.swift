ed3f1af6281a7969674ee86e37cac7747c297f1e
Ranveer <ranbanrambo@gmail.com>
2022-04-08 11:01:38 +1000

    🍴 Added trivial conformance to decodable
    Note that conformance is incomplete, and serves to allow a unit test to be conducted.

--- MythicWeather/Models/WeatherAPIResponse.swift
+++ MythicWeather/Models/WeatherAPIResponse.swift
@@ -11,8 +11,8 @@ import CoreLocation
 /**
  Weather metrics as described in [Fields in API response](https://openweathermap.org/weather-data)
  - Todo:
-    1. Add rain and clouds properties
-    2. Make the object conform to `Codable`. Currently this is not possible a property within object is of type `CLLocationCoordinate2D`, which does not conform to `Codable`
+ 1. Add rain and clouds properties
+ 2. Make the object conform to `Codable`. Currently this is not possible a property within object is of type `CLLocationCoordinate2D`, which does not conform to `Codable`
  - Author: [ranveerm](https://github.com/ranveerm) 👨🏾‍💻
  */
 struct WeatherAPIResponse {
@@ -31,5 +31,21 @@ struct WeatherAPIResponse {
         case coordinates = "coord"
         case weatherMetrics = "main"
         case weatherConditions = "weather"
+    }
+}
+
+// MARK: Protocol Conformances
+extension WeatherAPIResponse: Decodable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try values.decode(Int.self, forKey: .id)
+        self.date = try values.decode(Date.self, forKey: .date)
+        self.name = try values.decode(String.self, forKey: .name)
+        
+        self.coordinates = .init(latitude: 0, longitude: 0)
+        
+        self.weatherMetrics = try values.decode(WeatherMetrics.self, forKey: .weatherMetrics)
+        self.weatherConditions = try values.decode([WeatherConditions].self, forKey: .weatherConditions)
     }
 }
