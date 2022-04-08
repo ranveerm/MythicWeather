6b0e5a4771d5223306b73bc3422037eec9b932ce
Ranveer <ranbanrambo@gmail.com>
2022-04-08 11:36:04 +1000

    🧪 Added test for CLLocationCoordinate2D Equatable conformance

--- MythicWeatherTests/MythicWeatherTests.swift
+++ MythicWeatherTests/MythicWeatherTests.swift
@@ -6,31 +6,22 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import MythicWeather
 
 class MythicWeatherTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_CLLocationCoordinate2DEquatableConformance() throws {
+        /// Given
+        let baseCoordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+        
+        /// When
+        let identicalToBaseCoordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+        let differentToBaseCoordinate = CLLocationCoordinate2D(latitude: 1, longitude: 0)
+        let differentToBaseCoordinateAlt = CLLocationCoordinate2D(latitude: 0, longitude: 1)
+        
+        /// Then
+        XCTAssertEqual(baseCoordinate, identicalToBaseCoordinate)
+        XCTAssertNotEqual(baseCoordinate, differentToBaseCoordinate)
+        XCTAssertNotEqual(baseCoordinate, differentToBaseCoordinateAlt)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
