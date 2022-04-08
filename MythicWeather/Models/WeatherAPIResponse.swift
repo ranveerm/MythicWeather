b60b0383aadfc565507e33bf477db386f426d3f0
Ranveer <ranbanrambo@gmail.com>
2022-04-08 16:01:29 +1000

    ➕ Added weather API response object
    Note that e848701001b8d36ce601682f14b354261865b81a has the same commit message. See 607d1ec4a884969b73342a93800e99f64920e948 for the reason for this.

--- MythicWeather/Models/WeatherAPIResponse.swift
+++ MythicWeather/Models/WeatherAPIResponse.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+/**
+ Object to model JSON data from API response. See https://testapi.io/api/olestang/weather/list for an example.
+
+ - Author: [ranveerm](https://github.com/ranveerm) 👨🏾‍💻
+ */
+struct WeatherAPIResponse: Decodable, Equatable {
+    let responseCode: String
+    let calculatedTime: Double
+    let count: Int
+    let weatherItems: [Weather]
+    
+    enum CodingKeys: String, CodingKey {
+        case responseCode = "cod"
+        case calculatedTime = "calctime"
+        case count = "cnt"
+        case weatherItems = "list"
+    }
+}
