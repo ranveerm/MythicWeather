7079d59a785dd8f71e50edb971aede180e57f6c0
Ranveer <ranbanrambo@gmail.com>
2022-04-09 09:23:48 +1000

    🧪 Added test to verify floating point representation conversion
    See 7fd30c80a106a3eccdbf9e7a58f6b034304e6c00 for more details.

--- MythicWeatherTests/StandardLibraryExtensionTests.swift
+++ MythicWeatherTests/StandardLibraryExtensionTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import MapKit
+@testable import MythicWeather
 
 class StandardLibraryExtensionTests: XCTestCase {
     func test_CLLocationCoordinate2DEquatableConformance() throws {
@@ -22,5 +23,21 @@ class StandardLibraryExtensionTests: XCTestCase {
         XCTAssertEqual(baseCoordinate, identicalToBaseCoordinate)
         XCTAssertNotEqual(baseCoordinate, differentToBaseCoordinate)
         XCTAssertNotEqual(baseCoordinate, differentToBaseCoordinateAlt)
+    }
+    
+    func test_metricDisplayRepresentation() throws {
+        /// Given
+        let mockMetric = 8.230001
+        let mockMetricAlt = 8.0
+        let expectedMockMetric = "8.23"
+        let expectedMockMetricAlt = "8.00"
+        
+        /// When
+        let computedMockMetric = mockMetric.metricDisplayRepresentation
+        let computedMockMetricAlt = mockMetricAlt.metricDisplayRepresentation
+        
+        /// Then
+        XCTAssertEqual(expectedMockMetric, computedMockMetric)
+        XCTAssertEqual(expectedMockMetricAlt, computedMockMetricAlt)
     }
 }
