671551eaa2d6ed8e0ddeb1a2b60eee9f9e08f7fd
Ranveer <ranbanrambo@gmail.com>
2022-04-09 17:11:00 +1000

    ➕ Added enum to encapsulate App-wide constants

---
+++ MythicWeather/Data/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MythicWeather
+//
+//  Created by Ranveer Mamidpelliwar on 9/4/2022.
+//
+
+import Foundation
+
+enum Constants {
+    static let weatherDataURLString = "https://testapi.io/api/olestang/weather/list"
+}
